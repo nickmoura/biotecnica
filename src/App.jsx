4298b7b178f1c05e9fe1ce05d296366a52ba023e
Nick Moura <nickolas.moura@outlook.com>
2024-12-22 16:08:30 -0300

    BIOT-35 estrutura da pagina de produtos criada

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Products from "./pages/Products"
 
 const App = () => {
   return (
     <Router>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produtos" element={<Products />} />
+      </Routes>
       <Footer />
     </Router>
   );
---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import { Filter } from "../components/Filter"
+
+const Products = () => {
+	return (
+		<main className="container">
+			<h1>Catálogo de produtos</h1>
+		</main>
+	)
+}
+
+export default Products
